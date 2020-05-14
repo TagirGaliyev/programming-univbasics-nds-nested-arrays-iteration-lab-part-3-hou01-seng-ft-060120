2c1020a5a2fd7579c9c58e028a31d6ebed68ab5d
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-14 21:15:13 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,3 +8,18 @@ def join_nested_strings(src)
   end
   message
 end
+
+mixed_data_1 = [
+  ["The", 4, "quick"],
+  [-1, "brown", "fox", 30],
+  ["studied", 101, 233, "Ruby"]
+]
+
+mixed_data_2 = [
+  ["Turning", "and", "turning", "in", "the", "widening", "gyre"],
+  ["The", "falcon", "cannot", "hear", "the", "falconer;"],
+  ["Things", "fall", "apart;", "the", "centre", "cannot", "hold"]
+]
+
+puts join_nested_strings(mixed_data_1)
+puts join_nested_strings(mixed_data_2)
